7eedf6b33e1de963219e079f51fe3dd6fb7a3e7b
linkredon <rodrigoredon@gmail.com>
2025-07-10 10:09:34 -0300

    VERSÃO 10/07 - ALPHA REGIÃO

--- lib/demoMode.ts
+++ lib/demoMode.ts
@@ -11,12 +11,12 @@ export const isDemoMode = () => {
 // Obter configurações do Amplify das variáveis de ambiente
 export const getAmplifyConfig = () => {
   return {
-    aws_project_region: process.env.NEXT_PUBLIC_REGION || 'us-east-1',
+    aws_project_region: process.env.NEXT_PUBLIC_REGION || 'us-east-2',
     aws_appsync_graphqlEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT,
-    aws_appsync_region: process.env.NEXT_PUBLIC_REGION || 'us-east-1',
+    aws_appsync_region: process.env.NEXT_PUBLIC_REGION || 'us-east-2',
     aws_appsync_authenticationType: 'API_KEY',
     aws_appsync_apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    aws_cognito_region: process.env.NEXT_PUBLIC_REGION || 'us-east-1',
+    aws_cognito_region: process.env.NEXT_PUBLIC_REGION || 'us-east-2',
     aws_user_pools_id: process.env.NEXT_PUBLIC_USER_POOL_ID,
     aws_user_pools_web_client_id: process.env.NEXT_PUBLIC_USER_POOL_WEB_CLIENT_ID,
     aws_cognito_username_attributes: ['EMAIL'],
