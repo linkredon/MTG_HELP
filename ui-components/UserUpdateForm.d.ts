f81d02e81b3cbdc3b79277aa505cef9aa6ac835f
linkredon <rodrigoredon@gmail.com>
2025-07-10 23:09:32 -0300

    VERSÃO 10/07 - ALPHA BANGUERS

---
+++ ui-components/UserUpdateForm.d.ts
@@ -0,0 +1,68 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { GridProps, TextFieldProps } from "@aws-amplify/ui-react";
+import { User } from "../lib/API.ts";
+export declare type EscapeHatchProps = {
+    [elementHierarchy: string]: Record<string, unknown>;
+} | null;
+export declare type VariantValues = {
+    [key: string]: string;
+};
+export declare type Variant = {
+    variantValues: VariantValues;
+    overrides: EscapeHatchProps;
+};
+export declare type ValidationResponse = {
+    hasError: boolean;
+    errorMessage?: string;
+};
+export declare type ValidationFunction<T> = (value: T, validationResponse: ValidationResponse) => ValidationResponse | Promise<ValidationResponse>;
+export declare type UserUpdateFormInputValues = {
+    name?: string;
+    email?: string;
+    avatar?: string;
+    role?: string;
+    joinedAt?: string;
+    collectionsCount?: number;
+    totalCards?: number;
+    achievements?: string[];
+};
+export declare type UserUpdateFormValidationValues = {
+    name?: ValidationFunction<string>;
+    email?: ValidationFunction<string>;
+    avatar?: ValidationFunction<string>;
+    role?: ValidationFunction<string>;
+    joinedAt?: ValidationFunction<string>;
+    collectionsCount?: ValidationFunction<number>;
+    totalCards?: ValidationFunction<number>;
+    achievements?: ValidationFunction<string>;
+};
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type UserUpdateFormOverridesProps = {
+    UserUpdateFormGrid?: PrimitiveOverrideProps<GridProps>;
+    name?: PrimitiveOverrideProps<TextFieldProps>;
+    email?: PrimitiveOverrideProps<TextFieldProps>;
+    avatar?: PrimitiveOverrideProps<TextFieldProps>;
+    role?: PrimitiveOverrideProps<TextFieldProps>;
+    joinedAt?: PrimitiveOverrideProps<TextFieldProps>;
+    collectionsCount?: PrimitiveOverrideProps<TextFieldProps>;
+    totalCards?: PrimitiveOverrideProps<TextFieldProps>;
+    achievements?: PrimitiveOverrideProps<TextFieldProps>;
+} & EscapeHatchProps;
+export declare type UserUpdateFormProps = React.PropsWithChildren<{
+    overrides?: UserUpdateFormOverridesProps | undefined | null;
+} & {
+    id?: string;
+    user?: User;
+    onSubmit?: (fields: UserUpdateFormInputValues) => UserUpdateFormInputValues;
+    onSuccess?: (fields: UserUpdateFormInputValues) => void;
+    onError?: (fields: UserUpdateFormInputValues, errorMessage: string) => void;
+    onChange?: (fields: UserUpdateFormInputValues) => UserUpdateFormInputValues;
+    onValidate?: UserUpdateFormValidationValues;
+} & React.CSSProperties>;
+export default function UserUpdateForm(props: UserUpdateFormProps): React.ReactElement;
